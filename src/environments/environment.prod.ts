b11e2c4639f543e2909034ff678fd13c10212192
Sebastien Raimondi <sebastien.raimondi@gmail.com>
2023-12-03 01:22:38 +0100

    Url de production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  apiUrl: "https://soredi.crealcs.com/api",
+  ActApiUrl: "https://soredi.crealcs.com/api/activity",
+  AcrApiUrl: "https://soredi.crealcs.com/api/actreport",
+  EmpApiUrl: "https://soredi.crealcs.com/api/employee",
+  PhoApiUrl: "https://soredi.crealcs.com/api/pubholiday",
+  InvApiUrl: "https://soredi.crealcs.com/api/invoice",};
